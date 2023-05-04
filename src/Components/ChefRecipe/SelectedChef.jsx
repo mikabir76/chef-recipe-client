0597513350e37c5c1afb18a485bbe76a10d067b2
mikabir76 <kabirmd0987@gmail.com>
2023-05-04 20:07:26 +0600

    loading progress added

--- src/Components/ChefRecipe/SelectedChef.jsx
+++ src/Components/ChefRecipe/SelectedChef.jsx
@@ -5,17 +5,19 @@ const SelectedChef = ({chef}) => {
     const {id, chefName, chefPicture, yearsOfExperience}  = chef
     // console.log(chef)
     return (
-        <div className=''>
-          <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src={chefPicture} alt="Shoes" /></figure>
-  <div className="card-body">
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+  <figure className="px-10 pt-10">
+    <img src={chefPicture} alt="Shoes" className="rounded-xl" />
+  </figure>
+  <div className="card-body items-center text-center">
     <h2 className="card-title">{chefName}</h2>
     <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary"><FaArrowRight></FaArrowRight> Recipes</button>
+    <div className="card-actions">
+      <button className="btn btn-primary">Buy Now</button>
     </div>
   </div>
-</div>  
+</div>
         </div>
     );
 };
--- src/Main/Main.jsx
+++ src/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import NavigationBar from '../Layouts/NavigationBar/NavigationBar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 import Footer from '../Components/Footer/Footer';
 
 const Main = () => {
+    const navigation = useNavigation();
     return (
         <div>
             <NavigationBar></NavigationBar>
+            <div>{navigation.state === 'loading' && '<progress className="progress progress-info w-56" value="100" max="100"></progress>'}</div>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
